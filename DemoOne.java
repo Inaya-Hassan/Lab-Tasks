8c9252c188e308c7f91f40169a9ce883f44c2a07
Inaya <inayahassan0@gmail.com>
2025-09-11 10:25:26 +0500

    Accidently forced pushed ,uploading again

---
+++ DemoOne.java
@@ -0,0 +1,6 @@
+public class DemoOne {
+    public static void main(String[] args) {
+        Student p = new Student("Inaya", "inaya@gmail.com", 4.0);
+        p.display();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student{
+	String name;
+	String email;
+	double cgpa;
+
+Student(String name, String email, double cgpa){
+	this.name=name;
+	this.email=email;
+	this.cgpa=cgpa;
+}
+
+void display(){
+	System.out.println("Name: ");
+	System.out.println("Email: ");
+	System.out.println("Cgpa: ");
+	
+	}
+}
